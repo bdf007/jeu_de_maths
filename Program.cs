31351f2788b040f6f0fc9186e98d408c0186783e
bdf007 <bdf007@gmail.com>
2023-11-30 09:55:51 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace jeu_de_maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
